8914e11de754f280c80635c666c380e5f5c6ae06
code-Gambler <spillay6@myseneca.ca>
2024-01-29 21:44:15 -0500

    Updated Eslintrc.js To include Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
+  "env": {
+    "commonjs": true,
+    "es2021": true,
+    "node": true,
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    "jest": true
+  },
+  "extends": "eslint:recommended",
+  "overrides": [
+    {
+      "env": {
         "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
     }
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  },
+  "rules": {
+  }
 }
